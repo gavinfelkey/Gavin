d5cb89a40607fe4d0d52c3cc45b6b68c1dc54bca
Gavin Felkey <gavin_felkey@berkeley.edu>
2026-09-20 21:27:04 -0700

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,66 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+# Git is a version control system that allows you to track changes in your code and collaborate with others. GitHub is a web-based platform that hosts Git repositories and provides additional features for collaboration, such as issue tracking and pull requests. Git Bash is a command line interface that allows you to use Git commands on Windows
+
+# 2) What is the difference between the terminal and the command line?
+# A terminal is a program that provides a text-based interface for interacting with the operating system, while the command line is the interface itself where you can type commands to execute them.
+
+# 3) How does Windows PowerShell differ from Git Bash?
+# PowerShell is a terminal for windows that is pre built in and Git Bash is a terminal used by any operating system by installing it to be able to use git commands and function closer to zsh.
+
+# 4) What's the difference between Anaconda, conda, and Python?
+# Anaconda is a compilation of python and R and others, conda is a command line package manager that comes from anaconda, and python is a coding language.
+
+# 5) What is VS Code?
+# VS Code is a code editor that allows you to write and edit code in various programming languages as well as provides helpful color coding and otehr features to assist the user in writing it.
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+# Jupyter Notebook is a single-document interface focused on editing one file at a time, while JupyterLab is a multi-document workspace that lets you manage multiple notebooks, terminals, and text files side-by-side.
+
+# 7) What does ~/ mean?
+# ~/ is a shortcut that represents the home directory of the current user. It is used to move you to your home directory in one quick command.
+
+# 8) What's the difference between an absolute path and a relative path?
+# Absolute paths specify the full path to a file or directory starting from the root directory, while relative paths specify the location of a file or directory in relation to the current working directory.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+# /Users/Gavin/python_decal_fa26/Gavin/course_assignments/homework2 and the relative path would be ./course_assignments/homework2
+
+# 10) What command lets you move from "course_assignments/homework2" to "course_assignments/"?
+# cd ..
+
+# 11) What would rm ./ do in your current directory? (Don't try it!)
+# rm ./ would remove the current directory and everything inside of it
+
+# 12) What do the following commands do?
+# git add - this command stages changes in your working directory in anticipation for the next time you do git commit
+# git commit - this command takes the staged changes and saves them to the local repository with the option of writing a message to explain what you commited when you did
+# git push - this command takes the changes you have commited and pushes them to the remote repository on GitHub so that you or others can see changes made
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+# git add . adds all changes in current directory, while the other adds only the specified file
+
+# 14) What do "git status" and "git log -1" do?
+# git status shows current changes in the working directory and staging area, while git log -1 shows the most recent commit made in the repository.
+
+# 15) What's the difference between cloning a repository and pulling from it?
+# Cloning a repository creates a copy on your own computer, while pulling from a repository updates your local copy with the latest changes from the remote repository
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+# the most frustrating error in this class so far was when I was trying to follow along in class but I had not downloaded python i guess so I was unable to run something in VS code following along in class 3 or 4 i think
+
+# 17) What's a question you still have? What's something you're confused about?
+# In my files instead of main some of my files say master instead, I looked it up and it said master is a term for the same thing but as changed a while back or so?
+
+# 18) Tell me a fun fact!
+# I am a fraternal twin with my sister kira, ask me about her and id love to see if you think we look alike or not!
+
+# 19) Print your favorite math expression you've learned in Python so far.
+# (Hint: Use print() and add a comment explaining what it does.)
+print(5 % 2) # This prints the remainder when 5 is divided by 2 which is 1, the fact such a simple command does this was very fascinating to me.
